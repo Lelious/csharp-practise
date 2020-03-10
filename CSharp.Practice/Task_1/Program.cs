23bf3302a3d168912675e8c696b969cac383511a
Kris <moszechok@gmail.com>
2020-03-10 21:23:11 +0300

    Completed 1 task

---
+++ CSharp.Practice/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short valShort = 5;
+            object obj = valShort;
+            System.SByte valSbyte = (sbyte)(short)obj;
+            Console.WriteLine(valSbyte);
+            Console.WriteLine(valSbyte.GetType());
+            Console.ReadKey();
+        }
+    }
+}
