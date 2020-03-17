842600fc199013858535de8dce9d87981e469b31
ArtemShnip <tjoma27@gmail.com>
2020-03-17 21:52:32 +0300

    Completed 2 task

---
+++ Lect5.Greetings/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message:");
+            string str = Console.ReadLine();
+            int valueLength = str.Length;
+            Console.WriteLine($ "Count of numbers =" + valueLength);
+        }
+    }
+}
