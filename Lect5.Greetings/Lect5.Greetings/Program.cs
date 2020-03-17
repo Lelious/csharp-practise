e19ec1035a7a5308e7327600316cf6119034dfeb
ArtemShnip <tjoma27@gmail.com>
2020-03-17 21:47:26 +0300

    Completed 1 task

---
+++ Lect5.Greetings/Lect5.Greetings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lect5.Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataValue = DateTime.Now;
+            int hour = dataValue.Hour;
+
+            if (hour >= 9 && hour <= 12)
+            {
+                Console.WriteLine("Good Morning!");
+            }
+            if (hour >= 12 && hour <= 15)
+            {
+                Console.WriteLine("Good Day!");
+            }
+            if (hour >= 15 && hour <= 21)
+            {
+                Console.WriteLine("Good Evening!");
+            }
+            else
+            {
+                Console.WriteLine("Something!");
+            }
+
+
+        }
+    }
+}
