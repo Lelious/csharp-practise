89a32a3ad98432c3137ca6328ca061e41eede09d
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-04-23 20:00:27 +0300

    Complete 1 task

---
+++ Lect14.Task1/Lect14.Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lect14.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World!";
+            text.StringUpdate();
+        }
+    }
+}
